a36237911a11c9b13d4f3b8fe85bf409e93c18c4
Mario Monzón <mariomonzonmeza@gmail.com>
2025-08-10 16:00:01 -0600

    se modificó app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+console.log("Es una preba de consola") ;
+console.log('segunda prueba por el life saver') ;
