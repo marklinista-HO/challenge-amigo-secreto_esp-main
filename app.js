826219155660c02e59abe2890218d87d096f16d9
Mario Monzón <mariomonzonmeza@gmail.com>
2025-08-11 15:59:53 -0600

    Se implementa funcio agregarAmigo

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 console.log("Es una preba de consola") ;
-console.log('segunda prueba por el life saver') ;
+
+
+let largoDelista = 0 ;
+let listaNombres = [] ;
+let nombreLeido = "" ;
+
+largoDelista = listaNombres.length ;
+
+
+function limpiarTexto() {
+    document.querySelector('#amigo').value = '';
+}
+
+
+function agregarAmigo() {
+
+    // Lee nombre de amigo
+    nombreLeido = document.getElementById('amigo').value;
+
+
+    if (nombreLeido == "") {
+
+        alert(`Por favor, inserte un nombre`);
+
+    } else {
+        listaNombres.push(nombreLeido);
+        limpiarTexto() ;
+    }
+
+    
+    largoDelista = listaNombres.length ;
+    console.log(`largoDelista ${largoDelista}`) ;
+    console.log(listaNombres) ;
+
+}
+
+function sortearAmigo() {
+
+    alert(`oprimió Sortear amigo`);
+
+}
+
+
+
+largoDelista = listaNombres.length ;
+
+console.log(`largoDelista ${largoDelista}`) ;
+console.log(listaNombres) ;
+
+alert(`hola a ver que sale:  ${listaNombres}`);
