649088b7d6164cc76078e45cc918323658df8bec
Mario Monzón <mariomonzonmeza@gmail.com>
2025-08-15 19:41:15 -0600

    se implementó función que sortea nombres

--- app.js
+++ app.js
@@ -1,64 +1,71 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-console.log("Es una preba de consola") ;
 
-
-let largoDelista = 0 ;
+let largoDeLista = 0 ;
 let listaNombres = [] ;
 let nombreLeido = "" ;
 
 let indiceSorteado = 0 ;
-largoDelista = listaNombres.length ;
 
-indiceSorteado=  Math.floor(Math.random()*largoDelista)+1;
-
-
-
+//  limpia en contenido de la casilla amigo
 function limpiarTexto() {
     document.querySelector('#amigo').value = '';
 }
 
-
+//  función accionada por el botón "Añadir"
+//  Agregar nombre de amigo a la lista
 function agregarAmigo() {
 
     // Lee nombre de amigo
     nombreLeido = document.getElementById('amigo').value;
 
-
+    //  Revisa si se ingresó nombre
     if (nombreLeido == "") {
 
         alert(`Por favor, inserte un nombre`);
 
     } else {
+
+        //  Si se ingresó nombre, se agrega a la lista y se limpia la casilla
         listaNombres.push(nombreLeido);
         limpiarTexto() ;
     }
 
-    
-    largoDelista = listaNombres.length ;
-    console.log(`largoDelista ${largoDelista}`) ;
+    //  Se actualiza el largo de la lista
+    largoDeLista = listaNombres.length ;
+
+    console.log(`largoDelista ${largoDeLista}`) ;
     console.log(listaNombres) ;
 
 }
 
+//  función para desactivar botones una vez haya sorteado
+
+
+//  función accionada por el botón "Sortear amigo"
+//  Agregar nombre de amigo a la lista
 function sortearAmigo() {
  
-if (largoDelista > 0 ) {
+    if (largoDeLista > 0 ) {
 
+        //  Genera un número entero entre 0 y largoDeLista
+        indiceSorteado=  Math.floor(Math.random()*largoDeLista);
+    
 
-    indiceSorteado=  Math.floor(Math.random()*largoDelista);
-  
-    alert(`oprimió Sortear amigo ${indiceSorteado} ${listaNombres[indiceSorteado]}`);
-    console.log(`indice sorteado = ${indiceSorteado} ${listaNombres[indiceSorteado]}`) ;
+        console.log(`indice sorteado = ${indiceSorteado} ${listaNombres[indiceSorteado]}`) ;
 
+        // Desplegar nombre de amigo sorteado
+        let elementoHTML = document.getElementById("resultado");
+        elementoHTML.innerHTML = `Amigo secreto es: ${listaNombres[indiceSorteado]}`;
 
-//    asignarTextoElemento('ul','Juego del número ul');
-//    asignarTextoElemento('il',`Indica un número il`);
-} else {
+        // Desplegar nombre de amigo sorteado; prueba 2    
+        //  document.getElementById("resultado").innerHTML = `Amigo secreto es: ${listaNombres[indiceSorteado]}`;
 
-    alert(`Atención!   No ha ingresado nombres `);
+    } else {
 
-}
+        alert(`Atención!   No ha ingresado nombres `);
+        
+    }
 
 
 }
@@ -72,12 +79,8 @@ function asignarTextoElemento(elemento, texto) {
 
 
 
-
-
-
-largoDelista = listaNombres.length ;
-
-console.log(`largoDelista ${largoDelista}`) ;
+//  quitar !!!!!
+console.log(`largoDelista ${largoDeLista}`) ;
 console.log(listaNombres) ;
 
-alert(`hola a ver que sale:  ${listaNombres}`);
+
